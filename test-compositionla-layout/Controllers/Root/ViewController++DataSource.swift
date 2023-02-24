c4cac553d5402ddbaa3505b2f61fe84cef6a1d6b
kiarash vosough <vosough.k@gmail.com>
2023-02-24 09:52:29 +0330

    enhanced registeration

--- test-compositionla-layout/Controllers/Root/ViewController++DataSource.swift
+++ test-compositionla-layout/Controllers/Root/ViewController++DataSource.swift
@@ -8,33 +8,41 @@
 import class UIKit.UICollectionReusableView
 import class UIKit.UICollectionViewDiffableDataSource
 import class UIKit.UICollectionView
+import class UIKit.UICollectionViewCell
+import class UIKit.UIImage
 import struct Foundation.IndexPath
 
 extension ViewController {
     
     func generateDataSource(for collectionView: UICollectionView) -> UICollectionViewDiffableDataSource<Section, Item> {
+        
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewCell, Item> { cell, indexPath, item in
+            
+            var content = cell.defaultBackgroundConfiguration()
+            content.cornerRadius = 10
+            content.image = UIImage(contentsOfFile: item.imageURL.path)
+            cell.backgroundConfiguration = content
+        }
+
+        let supplementaryViewRegistration = UICollectionView.SupplementaryRegistration<HeaderView>(elementKind: HeaderView.elementKind) { supplementaryView, elementKind, indexPath in
+            supplementaryView.text = Section.allCases[indexPath.section].rawValue.capitalized
+        }
+
         let dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) { collectionView, indexPath, item in
+
             let sectionType = Section.allCases[indexPath.section]
             switch sectionType {
             case .myAlbum, .triplet, .lowHeight:
-                guard let cell = collectionView.dequeueReusableCell(
-                    withReuseIdentifier: ImageCell.reuseIdentifier,
-                    for: indexPath
-                ) as? ImageCell else { fatalError("Could not create new cell") }
-                cell.featuredPhotoURL = item.imageURL
-                return cell
+                return collectionView.dequeueConfiguredReusableCell(
+                    using: cellRegistration,
+                    for: indexPath,
+                    item: item
+                )
             }
         }
-        
+
         dataSource.supplementaryViewProvider = { (collectionView: UICollectionView, kind: String, indexPath: IndexPath) -> UICollectionReusableView? in
-            
-            guard let supplementaryView = collectionView.dequeueReusableSupplementaryView(
-                ofKind: kind,
-                withReuseIdentifier: HeaderView.reuseIdentifier,
-                for: indexPath
-            ) as? HeaderView else { fatalError("Cannot create header view") }
-            supplementaryView.text = Section.allCases[indexPath.section].rawValue.capitalized
-            return supplementaryView
+            return collectionView.dequeueConfiguredReusableSupplementary(using: supplementaryViewRegistration, for: indexPath)
         }
         
         return dataSource
